7a9746a54177f7212073aa0ba0b19265330bb5f7
Robin H. Johnson <rjohnson@sitka.bclibraries.ca>
2013-05-24 00:09:21 -0700

    Working S3 testcase.
    Signed-off-by: Robin H. Johnson <rjohnson@sitka.bclibraries.ca>

--- database_constants.php
+++ database_constants.php
@@ -12,12 +12,12 @@ if(file_exists($lf)) {
  *  \/  the MySQL database and user that you have created for OpenBiblio.   \/
  *********************************************************************************
  */
-defined("OBIB_HOST") || define("OBIB_HOST"     "localhost");
-defined("OBIB_DATABASE") || define("OBIB_DATABASE" "DB_library");
-defined("OBIB_USERNAME") || define("OBIB_USERNAME" "ob_user");
+defined("OBIB_HOST") || define("OBIB_HOST",     "localhost");
+defined("OBIB_DATABASE") || define("OBIB_DATABASE", "DB_library");
+defined("OBIB_USERNAME") || define("OBIB_USERNAME", "ob_user");
 defined("OBIB_PWD") || define("OBIB_PWD",      "abwa!");
-defined("OBIB_REMOTE_USER") || define("OBIB_REMOTE_USER" "circulation");
-defined("OBIB_REMOTE_USER_PWD") || define("OBIB_REMOTE_USER_PWD" "postalABWA");
+defined("OBIB_REMOTE_USER") || define("OBIB_REMOTE_USER", "circulation");
+defined("OBIB_REMOTE_USER_PWD") || define("OBIB_REMOTE_USER_PWD", "postalABWA");
 defined("ROOT_ARCHIVES_PATH") || define("ROOT_ARCHIVES_PATH", "/mnt/usb/books/");
 
 /*********************************************************************************
@@ -25,4 +25,16 @@ defined("ROOT_ARCHIVES_PATH") || define("ROOT_ARCHIVES_PATH", "/mnt/usb/books/")
  *  ||                                                                      ||
  *********************************************************************************
  */
+
+# New stuff
+$_CONFIG = array(
+	'aws_s3' => array(
+		'key' => 'CENSORED',
+		'secret' => 'CENSORED',
+		'base_url' => 'CENSORED',
+		# Custom
+		'bucket' => 'nnels',
+		'prefix' => 'Books',
+	),
+);
 ?>
--- s3.php
+++ s3.php
@@ -1,20 +1,26 @@
 <?php
 require 'AWSSDKforPHP/aws.phar';
+require 'database_constants.php';
 use Aws\S3\S3Client;
-$config = array(
-    'key'    => $_CONFIG['s3_key'],
-	'secret' => $_CONFIG['s3_secret'],
-);
+$config = $_CONFIG['aws_s3'];
 $s3v2 = S3Client::factory($config);
-$result = $s3v2->listBuckets();
-print_r($result);
+#$result = $s3v2->listBuckets();
+#print_r($result['Buckets']);
 
-$objName = 'popular/emma/emma.xml';
-$objInfo = $client->HeadObject(array(
-			'Bucket' => $_CONFIG['s3_bucket'], 
+function os_path_join() {
+  return preg_replace('~[/\\\]+~', DIRECTORY_SEPARATOR, implode(DIRECTORY_SEPARATOR, array_filter(func_get_args(), function($p) {
+    return $p !== '';
+  })));
+}
+
+$bucket = $_CONFIG['aws_s3']['bucket'];
+$prefix = $_CONFIG['aws_s3']['prefix'];
+$objName = os_path_join($prefix, 'popular/emma/emma.xml');
+$objInfo = $s3v2->HeadObject(array(
+			'Bucket' => $bucket,
 			'Key' => $objName));
-$obj = $client->GetObject(array(
-			'Bucket' => $_CONFIG['s3_bucket'], 
+$obj = $s3v2->GetObject(array(
+			'Bucket' => $bucket,
 			'Key' => $objName));
 
 echo $obj['Body'];
