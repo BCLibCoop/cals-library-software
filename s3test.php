15b04053aba9dba591f4498894cf46cf425de968
Robin H. Johnson <rjohnson@sitka.bclibraries.ca>
2013-05-24 12:35:23 -0700

    S3 test fun.

--- s3test.php
+++ s3test.php
@@ -9,10 +9,18 @@ $objInfo = s3_object_head('popular/emma/emma.xmlx');
 $f = 'popular/emma/emma.xml';
 $f = 'restricted/S00004/x00004_fortune_and_misfortune_are_both_a_blessing_S00004.zip';
 $lf = 's3://nnels/Books/'.$f;
-$objInfo = s3_object_head($f);
-print($objInfo['ContentLength']."\n");
+#$objInfo = s3_object_head($f);
+#print($objInfo['ContentLength']."\n");
 print(filesize($lf)."\n");
 print(file_exists($lf)."\n");
+
+$command = $s3v2->getCommand('GetObject', array(
+  'Bucket' => 'nnels',
+  'Key' => 'Books/'.$f,
+  'ResponseContentDisposition' => 'attachment; filename="data.txt"'
+));
+$signedUrl = $command->createPresignedUrl('+10 minutes');
+print($signedUrl);
 
 #$obj = s3_object_get('popular/emma/emma.xml');
 #print_r($objInfo['data:protected']);
