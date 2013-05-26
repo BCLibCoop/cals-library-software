2c4238a69f857420ee0000af2fd83aee99fb1628
Robin H. Johnson <rjohnson@sitka.bclibraries.ca>
2013-05-25 23:22:07 -0700

    S3 testing.

--- s3test.php
+++ s3test.php
@@ -6,20 +6,45 @@ $objInfo = s3_object_head('popular/emma/emma.xmlx');
 //var_dump($e);
 }
 
+global $_CONFIG;
+$bucket = $_CONFIG['aws_s3']['bucket'];
+$prefix = $_CONFIG['aws_s3']['prefix'];
+
 $f = 'popular/emma/emma.xml';
 $f = 'restricted/S00004/x00004_fortune_and_misfortune_are_both_a_blessing_S00004.zip';
-$lf = 's3://nnels/Books/'.$f;
+$f = 'pd/G17401/The_Time_Machine_G17401.zip';
+$lf = sprintf('s3://%s/%s/%s', $bucket, $prefix, $f);
 #$objInfo = s3_object_head($f);
 #print($objInfo['ContentLength']."\n");
 print(filesize($lf)."\n");
 print(file_exists($lf)."\n");
 
+print("Copy\n");
+$old_key = os_path_join($prefix, $f);
+$basename = pathinfo($old_key,PATHINFO_BASENAME);
+$uid = 12345;
+$copyid = 12345;
+$tmp_key =  sprintf('downloads/user-%08x-copy-%08x/%s', $uid, $copyid, $basename);
+$expiry_time = '+1 minutes';
+
+try {
+$command = $s3v2->copyObject(array(
+  'CopySource' => os_path_join($bucket, $old_key),
+  'Key' => $tmp_key,
+  'Bucket' => $bucket,
+  'Expires' => $expiry_time,
+  'StorageClass' => 'REDUCED_REDUNDANCY',
+));
+} catch(Aws\S3\Exception\NoSuchKeyException $e) {
+printf("%s\n",$e->getResponse());
+}
+$expiry_time = '+5 minutes';
 $command = $s3v2->getCommand('GetObject', array(
-  'Bucket' => 'nnels',
-  'Key' => 'Books/'.$f,
+  'Bucket' => $bucket,
+  'Key' => $tmp_key,
   'ResponseContentDisposition' => 'attachment; filename="data.txt"',
 ));
-$signedUrl = $command->createPresignedUrl('+10 minutes');
+$signedUrl = $command->createPresignedUrl($expiry_time);
 print($signedUrl);
 
 #$obj = s3_object_get('popular/emma/emma.xml');
